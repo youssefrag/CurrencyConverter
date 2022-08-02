545a108566dca253e1db9558a999772eecf84447
Youssef Ragab <ragab.youssef95@gmail.com>
2022-08-02 13:55:19 -0400

    add axios to client

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,17 @@
+import { Button } from "@mui/material";
+
 function App() {
+
+  const handleSubmit = () => {
+    
+  }
+
   return (
     <div className="App">
       <h1>Currency Converter</h1>
+      <Button>
+        Get rates
+      </Button>
     </div>
   );
 }
